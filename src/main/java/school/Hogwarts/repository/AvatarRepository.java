6f17fbc36e246e5094bdaeefc79ed87613b6cc03
master <aleshenko_ira_2017@mail.ru>
2025-11-10 13:06:03 +0300

    SQL

--- src/main/java/school/Hogwarts/repository/AvatarRepository.java
+++ src/main/java/school/Hogwarts/repository/AvatarRepository.java
@@ -1,11 +1,12 @@
 package school.Hogwarts.repository;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import school.Hogwarts.model.Avatar;
 
-import java.awt.print.Pageable;
+
 import java.util.Optional;
 
 @Repository
